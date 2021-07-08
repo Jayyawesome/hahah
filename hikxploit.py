3d0fd16f8c678c0f0f04199ddfa109b6203d68c4
Jayyawesome <86467457+Jayyawesome@users.noreply.github.com>
2021-07-08 16:44:14 +0800

    Update hikxploit.py

--- hikxploit.py
+++ hikxploit.py
@@ -50,7 +50,6 @@ def usage():
                                                                 
 def gather_host_shodan():
   api_shodan_key = "my_apikey"
-    if api_shodan_key == "":
         print(t.red('no shodan api found, please insert a valid one'))
         api_shodan_key_to_file = raw_input('\ntype here:')
         with open(path + "/api.txt", "wb") as api:
